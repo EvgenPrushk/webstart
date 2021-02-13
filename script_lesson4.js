c7e22e4c2745b82b6ee364aa7a2703a1f3cc660c
EvgenPrushk <ogmprushak@mail.ru>
2021-02-13 16:09:46 +0300

    feat: add lesson#4

--- script_lesson4.js
+++ script_lesson4.js
@@ -87,3 +87,66 @@ if (18<= age && age <=25) {
 '5' === 5 // false
 NaN == NaN // false
 100 !== 99 //true
+'a' > "b" // false
+'a' < "b" // true
+'a'.charCodeAt(0) // 97
+'b'.charCodeAt(0) // 98
+'aa' < 'ab' // true
+
+
+//** Modal Window */
+
+alert('Hello'); // сообщает текст пользователю
+confirm("Вам есть 18 лет?") // ок - true, cansel - false
+prompt('Как Вас зовут?') // позволяет ввести ответ - функция позволяющая вести текст в виде строки
+
+userName = prompt('Как тебя зовут', 'Алексей'); // вторым аргументом вставляется по умолчанию значение
+switch(userName) {
+    case "Алексей": 
+    console.log('Алексей -  имя означает "защитник');
+    break;
+    case "Сергей":
+    console.log('Сергей -  имя означает "высокий');
+    break;
+    case "Тимофей":
+    console.log('Тимофей - имя означает "почитающий бога');
+    break;
+    default:
+        console.log('я не знаю значение этого имени');
+}
+
+alert('Приветствую тебяя в нашем приложении');
+const userName = prompt('Как тебя зовут', 'Алексей');
+alert(userName + 'какое красивое имя!');
+const answer = confirm("Вам все понравилось?");
+if (answer) {
+    alert(' Рады, что наш сервис Вам понравился')
+} else {
+    'Будем работать над ошибкой'
+}
+
+const number1 = parseFloat(prompt('Введите первое число:'));
+const number2 = parseFloat(prompt('Введите второе число:'));
+const operation = prompt('Введите операцию', 'сложение')
+switch(userName) {
+    case "сложение": 
+    case "Cложение": 
+    case "+": 
+        alert(number1 + number2)
+    break;
+    case "умножение":
+    case "Умножение":
+    case "*":
+    alert(number1 * number2)
+    break;
+    case "вычитание":
+    case "Вычитание":
+    case "-":
+    alert(number1 - number2)
+    break;
+    case "деление":
+    case "Деление":
+    case "/":
+        alert(number1 / number2)
+    break
+}
