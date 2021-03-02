8053f6d065f3a690b958564fe6463eb5e9b8fb37
EvgenPrushk <ogmprushak@mail.ru>
2021-03-02 10:27:54 +0300

    feat: add lesson 7

---
+++ script_lesson7.js
@@ -0,0 +1,12 @@
+let name1 = "Алексей";
+let name1 = "Алексей";
+let surname1 = "Danchin";
+let surname2 = "Danchin";
+let age1 = 20;
+let age2 = 20;
+let gender1 = "male";
+let gender2 = "male";
+// объект
+const person = {
+
+}
