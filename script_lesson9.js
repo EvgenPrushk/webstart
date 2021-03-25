902611102f4f3dd356accf6fbed1ad7e7bbdb3ad
EvgenPrushk <ogmprushak@mail.ru>
2021-03-26 01:15:38 +0300

    feat: ()=> {} function

--- script_lesson9.js
+++ script_lesson9.js
@@ -95,8 +95,161 @@
 //   return true;
 // }
 
-// Вызов функции после ее обьявления
-(function IIFE(a, b, c) {
-  console.log(a, b, c);
-})(1, 2, 3);
-//23-08
+// // Вызов функции после ее обьявления - устаревший вариант
+// (function IIFE(a, b, c) {
+//   console.log(a, b, c);
+// })(1, 2, 3);
+
+// ЛЕНИВАЯ ФУНКЦИЯ
+
+// можно не давать название
+// const x = function isSimple(n, a, b, ...args) {
+//   for (let i = 2; i < n; i++) {
+//     console.log({ i });
+//     if (n % i === 0) {
+//       // после ретерна функция прекращается
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+// const n = 15;
+// const v = x(n);
+// console.log(n, v);
+
+// ОБЛАСТЬ ВИДИМОСТИ ФУНКЦИИ
+// a();
+// b();
+// function a() {
+//   b();
+//   c();
+//   // это b видна из тела функции а
+//   function b () {
+//     console.log('b2');
+//   }
+// }
+// function b() {
+//   console.log('b1');
+// }
+// function c() {
+//   console.log('c1');
+// }
+
+// СТРАТЕГИЯ
+
+// const array = [1, 22, 33, 223, 32354, 5, 2, 3, 23, 4, 5, 2, 2, 2, 4];
+// const variant = confirm('хотите видеть четные числа?')
+// const sort = variant ? getEven : getOdd;
+// let sort = null;
+// if (variant) {
+//   sort = getEven;
+// } else {
+//   sort = getOdd;
+// }
+
+// const rezult = sort(array);
+// console.log(rezult);
+
+// function getOdd(array) {
+//   const rezult = [];
+
+//   for (const item of array) {
+//     if (item % 2 !== 0) {
+//       rezult.push(item);
+//     }
+//   }
+//   return rezult;
+// }
+
+// function getEven(array) {
+//   const rezult = [];
+
+//   for (const item of array) {
+//     if (item % 2 === 0) {
+//       rezult.push(item);
+//     }
+//   }
+//   return rezult;
+// }
+
+// КОНТЕКСТ
+
+// const user = {
+//   name: "Alex",
+//   surname: 'Belov',
+//   age: 34,
+//   getFullname() {
+//     //this ссылается на объект
+//     return `${this.name} ${this.surname}`;
+//   }
+// }
+// // function getFullname() {
+// //   return `${this.name} ${this.surname}`;
+// // }
+// // создаем ссылку на функцию в объекте user
+// // user.getFullname = getFullname;
+// const getFullname = user.getFullname; // повторная ссылка на объект
+// const fullname = getFullname();
+
+// console.log({user, fullname});
+
+// УПРАВЛЕНИЕ КОНТЕКСТОМ
+
+// const user = {
+//   name: "Alex",
+//   surname: "Belov",
+//   age: 34,
+// };
+
+// function getFullname() {
+//   //this ссылается на объект
+//   return `${this.name} ${this.surname}`;
+// }
+// // с помощью bind создаю новую функцию, которая ссылается на user
+// const getFullnameUser = getFullname.bind(user, 1, 2, 3);
+
+// const obj = {};
+
+// // болеее низкий приоритет приявязки
+// obj.getFullnameUser = getFullnameUser;
+
+// const fullname = obj.getFullnameUser(4, 5, 6);
+// console.log({ user, fullname });
+
+// const user = {
+//     name: "Alex",
+//     surname: "Belov",
+//     age: 34,
+//   };
+
+//   function getFullname() {
+//     //this ссылается на объект
+//     return `${this.name} ${this.surname}`;
+//   }
+//   // с помощью call создаем контекст и тут же вызываем функцию
+//   const fullname = getFullname.call(user);
+
+//   console.log({ user, fullname });
+
+// const user = {
+//     name: "Alex",
+//     surname: "Belov",
+//     age: 34,
+//   };
+
+//   function getFullname() {
+//     //this ссылается на объект
+//     return `${this.name} ${this.surname}`;
+//   }
+//   // с помощью call создаем контекст и тут же вызываем функцию, вторым аргументом передаем массив
+//   const fullname = getFullname.apply(user, [1, 2, 3, 4]);
+
+//   console.log({ user, fullname });
+
+// СТРЕЛОЧНАЯ ФУНКЦИЯ
+// если она возвращает результат, то кавычки можно опустить и если один аргумент, то можно опустить
+const getSum = (...xs) => xs.reduce((a, b) => a + b, 0);
+// нет свойство аргументом
+// нет this, есть только родительский
+console.log(getSum(11, 55, 44, 22, 33));
