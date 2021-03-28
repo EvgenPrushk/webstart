4518af70a7d3a5312a62dc727761d9347e091df5
EvgenPrushk <ogmprushak@mail.ru>
2021-03-28 16:33:26 +0300

    feat: lesson11

--- script_lesson11.js
+++ script_lesson11.js
@@ -87,24 +87,163 @@
 
 // ДИКАРАТОРЫ
 
-function debounce(callback, timer) {
-  // флаг, который фиксирует происходит ли отсчте от предыдущего вызова функции
-  let flag = false;
-  return function (...args) {
-    if (flag) {
-      clearTimeout(flag);
-    }
-    flag = setTimeout(() => {
-      callback(...args);
-      flag = false;
-    }, timer);
-  };
-}
+// function debounce(callback, timer) {
+//   // флаг, который фиксирует происходит ли отсчте от предыдущего вызова функции
+//   let flag = false;
 
-const button = document.querySelector("button");
-button.addEventListener(
-  "click",
-  debounce((e) => {
-    console.log("fired");
-  }, 1000)
+// вызывает функцию, которая это callback использует со своей бизнес логикой
+//   return function (...args) {
+//     if (flag) {
+//       clearTimeout(flag);
+//     }
+//     // ожидает, чтобы функция не вызывалась чаще чем время timer
+//     flag = setTimeout(() => {
+//       callback(...args);
+//       flag = false;
+//     }, timer);
+//   };
+// }
+
+// const input = document.querySelector("input");
+// input.addEventListener(
+//   "keyup",
+//   debounce((e) => {
+//     console.log(e.target.value);
+//   }, 500)
+// );
+
+// const button = document.querySelector("button");
+// button.addEventListener(
+//   "click",
+//   debounce((e) => {
+//     console.log("fired");
+//   }, 1000)
+// )
+
+// МЕТОДЫ КЛАССА
+
+//МУТИРУЮЩИЕ МЕТОДЫ = Изменяющие
+const users = [
+  {
+    id: 1,
+    name: "Ales",
+    surname: "Bink",
+    age: 77,
+  },
+  {
+    id: 2,
+    name: "Maik",
+    surname: "Mall",
+    age: 23,
+  },
+  {
+    id: 3,
+    name: "Ross",
+    surname: "Milk",
+    age: 24,
+  },
+];
+
+// users.splice(1, 1);
+// удалил
+// {
+//         id: 2,
+//         name: "Maik",
+//         surname: "Mall",
+//         age: 23,
+//       },
+
+// измененый массив
+// const users = [
+//         {
+//           id: 1,
+//           name: "Ales",
+//           surname: "Bink",
+//           age: 77,
+//         },
+//         {
+//           id: 3,
+//           name: "Ross",
+//           surname: "Milk",
+//           age: 24,
+//         },
+
+// ];
+
+// ПЕРЕБИРВЮЩИЕ МЕТОДЫ
+
+//позволяеют найти элемент, который будет отвечать требованиям
+// users.find(user => user.age === 23) //  выведет 2 элемент
+// const maxAge = Math.max(...users.map((x) => x.age));
+
+// ГЕНЕРИРУЮЩИЕ МЕТОДЫ
+// // Не мутируют исходный массив
+// // const names = user.map(user => user.name); ["Ales", "Maik", "Ross"]
+
+// // const ages = users.map(user => user.age)  [77, 23, 24]
+
+// // users.filter( x => x.age >22)
+// // 0: {id: 1, name: "Ales", surname: "Bink", age: 77}
+// // 1: {id: 2, name: "Maik", surname: "Mall", age: 23}
+// // 2: {id: 3, name: "Ross", surname: "Milk", age: 24}
+
+// //ДЛЯ ПОВЕРХНОСТНОГО КОПИРОВАНИЯ ИСПОЛЬЗУЕМ slice()
+// // users.slice(); // ссылки будут разные на массив разные, но на объекты одинаковые
+
+// // ЦЕПНОЙ СТИЛЬ ПРОГРАММИРОВАНИЯ
+
+// // const names = users.map((x => x.name));
+// // const shortNames = names.map((x) => x.slice(0, 3));
+// // const call = users.map((x) => x.name).map((x) => x.slice(0, 3));
+// // console.log(call);
+
+// // ПОЛИФИЛЫ = не поддерживается из коробки
+// console.log(
+//   users.map((x) => x.name),
+//   map(users, (x) => x.name)
+// );
+
+// // передаем
+// // users принимает, как контекст this.
+// // далее передаем callback and thisArg
+// function map(array, callback, thisArg) {
+//   // строчка для полифилов
+//   callback = thisArg ? callback.bind(thisArg) : callback;
+
+//   const rezult = [];
+//   // пробегаемся по всем элементам массива
+//   for (let i = 0; i < array.length; i++) {
+//     // элемент - результат выполнения функции и элемента массива
+//     // может принимать индекс элемента и лист аргумент(ссылку на массив)
+//     const item = callback(array[i], i, array);
+//     // отправляем результаты выполнения функции в результирующмй массив
+//     rezult.push(item);
+//   }
+
+//   return rezult;
+// }
+
+// ПОЛЕЗНЫЕ ФУНКЦИИ (LODASH)
+
+function chunk(array, size) {
+  const rezult = [];
+  let sum = [];
+  for (let i = 0; i < array.length; i++) {
+    sum.push(array[i]);
+    if (sum.length >= size) {
+      rezult.push(sum);
+      sum = [];
+    }
+  }
+
+  if (sum.length) {
+    rezult.push(sum);
+    sum = [];
+  }
+
+  return rezult;
+}
+console.log(
+
+  chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3)
 );
